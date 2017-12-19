5a86bbfd2b1edc27ecd17009b6a63d499fa84e30
Evan McClellan <vanlellan@gmail.com>
2017-12-19 15:45:55 -0500

    updated count32.vhd
    -added internal v1495 clock for Read signal edge detection
    -moved things around to decouple counting external clocks and outputting the count (i.e. 'Read')
    -Reset signal resets both the counter and the output and associated registers

--- mlu_design/SRC/v1495usr_demo/count32.vhd
+++ mlu_design/SRC/v1495usr_demo/count32.vhd
@@ -10,9 +10,11 @@ use ieee.numeric_std.all;
 entity count32 is
   port (
     --Clock
+    i_LCLK : in std_logic;
     i_Clk : in std_logic;
+
     i_Reset : in std_logic;
-    i_Trigger : in std_logic;
+    i_Read : in std_logic;
 
     --Output Bit
     o_Count : out unsigned (31 downto 0) := (others => '0')
@@ -26,33 +28,31 @@ architecture rtl of count32 is
 
   signal r_Count : unsigned (31 downto 0) := (others => '0');
   signal r_Output : unsigned (31 downto 0) := (others => '0');
-  signal r_OldTrig : std_logic := '0';
-  signal r_Trig : std_logic := '0';
+  signal r_OldRead : std_logic := '0';
+  signal r_Read : std_logic := '0';
 
 begin
+
+  p_Output : process(i_Read, i_Reset) is
+  begin
+    if i_Reset = '1' then
+      o_Count <= to_unsigned(0,32);
+      r_Output <= to_unsigned(0,32);
+    else
+      o_Count <= r_Output;
+      if rising_edge(i_Read) then
+        r_Output <= r_Count;
+      end if;
+    end if;
+  end process p_Count;
 
   p_Count : process(i_Clk, i_Reset) is
   begin
     if i_Reset = '1' then
       r_Count <= to_unsigned(0,32);
-      r_Output <= to_unsigned(0,32);
-      o_Count <= to_unsigned(0,32);
     elsif rising_edge(i_Clk) then
-      r_OldTrig <= r_Trig;
-      r_Trig <= i_Trigger;
-      if (r_OldTrig = '0' and r_Trig = '1') then
-        r_Output <= r_Count;
-      end if;
       r_Count <= r_Count + 1;
-      o_Count <= r_Output;
     end if;
   end process p_Count;
-
---  p_Output : process(i_Trigger) is
---  begin
---    if rising_edge(i_Trigger) and i_Reset = '0' then
---      r_Output <= r_Count;
---    end if;
---  end process p_Output;
 
 end architecture rtl;

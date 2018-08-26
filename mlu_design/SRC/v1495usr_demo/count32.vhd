14cc4ef06525f81261f1331a5ae4246d1ef3530f
Evan McClellan <vanlellan@gmail.com>
2018-08-26 00:58:11 -0400

    first order simulation looks good
    -

--- mlu_design/SRC/v1495usr_demo/count32.vhd
+++ mlu_design/SRC/v1495usr_demo/count32.vhd
@@ -33,7 +33,7 @@ architecture rtl of count32 is
   signal r_Buffer : buff_array := (others => to_unsigned(0,32));
 
   signal r_nStoredEvents   : unsigned(15 downto 0) := (others => '0');
-  signal r_maxStoredEvents : unsigned(15 downto 0) := (others => '0');
+  signal r_maxStoredEvents : unsigned(15 downto 0) := to_unsigned(BUFF_DEPTH+1,16);
   signal r_nMissedEvents   : unsigned(15 downto 0) := (others => '0');
   signal r_Count : unsigned (63 downto 0) := (others => '0');
   signal r_Output : unsigned (31 downto 0) := (others => '0');
@@ -66,22 +66,22 @@ begin
         r_EdgeRead <= r_Read and not r_OldRead;
 
 	--Read and trig rising edges
-        if (r_EdgeRead and r_EdgeTrig) then
+        if (r_EdgeRead='1' and r_EdgeTrig='1') then
           if (r_nStoredEvents > 0 and r_nStoredEvents < r_maxStoredEvents) then
             r_Output <= r_Buffer(0);
             r_Buffer(BUFF_DEPTH) <= to_unsigned(0,32);	--for the array shift, write a zero at the high-end
             for i in 0 to BUFF_DEPTH-1 loop
-              if i /= nStoredEvents:
+              if i /= r_nStoredEvents then
                 r_Buffer(i) <= r_Buffer(i+1);
               else
                 r_Buffer(i) <= r_Count(31 downto 0);
               end if;
             end loop;
-          elsif(r_nStoredEvents = 0)
+          elsif(r_nStoredEvents = 0) then
             r_Output <= to_unsigned(0,32);	--trying to read with no stored events, set output to zero
             r_Buffer(to_integer(r_nStoredEvents)) <= r_Count(31 downto 0);
             r_nStoredEvents <= r_nStoredEvents + 1;
-          elsif(r_nStoredEvents = r_maxStoredEvents)	--trying to trigger with full buffer, increment nMissedEvents
+          elsif(r_nStoredEvents = r_maxStoredEvents) then	--trying to trigger with full buffer, increment nMissedEvents
             r_nMissedEvents <= r_nMissedEvents + 1;
             r_Output <= r_Buffer(0);
             r_nStoredEvents <= r_nStoredEvents - 1;
@@ -92,11 +92,12 @@ begin
           end if;
 
 	--Read rising edge only
-        elsif (r_EdgeRead and not r_EdgeTrig) then
+        elsif (r_EdgeRead='1' and r_EdgeTrig='0') then
           if (r_nStoredEvents > 0) then
             r_Output <= r_Buffer(0);
             r_nStoredEvents <= r_nStoredEvents - 1;
-            for i in 0 to BUFF_DEPTH loop
+            r_Buffer(BUFF_DEPTH) <= to_unsigned(0,32);	--for the array shift, write a zero at the high-end
+            for i in 0 to BUFF_DEPTH-1 loop
               r_Buffer(i) <= r_Buffer(i+1);
             end loop;
           else
@@ -104,11 +105,11 @@ begin
           end if;
 
 	--Trig rising edge only
-        elsif (not r_EdgeRead and r_EdgeTrig) then
+        elsif (r_EdgeRead='0' and r_EdgeTrig='1') then
 	  if (r_nStoredEvents < r_maxStoredEvents) then
             r_Buffer(to_integer(r_nStoredEvents)) <= r_Count(31 downto 0);
             r_nStoredEvents <= r_nStoredEvents + 1;
-          elsif
+          else
             r_nMissedEvents <= r_nMissedEvents + 1;	--trying to trigger with full buffer, increment nMissedEvents
           end if;
         end if;
--- mlu_design/SRC/v1495usr_demo/go_count.sh
+++ mlu_design/SRC/v1495usr_demo/go_count.sh
@@ -1,7 +1,14 @@
-echo "Check 0"
+#echo "Check 0"
+#ghdl -i tb_Count32.vhd count32.vhd 
+#echo "Check 1"
+#ghdl -m count32_tb
+#echo "Check 2"
+#./count32_tb --wave=out.ghw
+#echo "Check 3"
+
+
 ghdl -i tb_Count32.vhd count32.vhd 
-echo "Check 1"
-ghdl -m count32_tb
-echo "Check 2"
-./count32_tb --wave=out.ghw
-echo "Check 3"
+ghdl -a count32.vhd
+ghdl -a tb_Count32.vhd
+ghdl -e count32_tb
+ghdl -r count32_tb --wave=out.ghw

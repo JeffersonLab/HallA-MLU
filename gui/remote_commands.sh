cd004bc2b4d7211b58979547e5e29eaddf9206f5
R Evan McClellan <randallm@jlab.org>
2018-12-17 16:48:08 -0500

    rename freq to mlurand, adjust xterm window size

--- gui/remote_commands.sh
+++ gui/remote_commands.sh
@@ -1,8 +1,8 @@
 
 ssh -t -t intelha3 << EOF
-echo "Got Here!"
 cd /adaqfs/home/adaq/MLUright/driver
 python /adaqfs/home/adaq/HallA-MLU/gui/server.py
+echo "MLU Random Pulser Control Server Closed."
 exit
 EOF
 exit

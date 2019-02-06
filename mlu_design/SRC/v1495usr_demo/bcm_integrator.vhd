acb209085074af0bcc9f1937ac222db2011dcdfd
Evan McClellan <vanlellan@gmail.com>
2019-02-06 16:07:44 -0500

    update, compile, implement version 0x21D
    -added a buffer for current/sum readout, to eliminate the possibility of timing effects
    (i.e. CODA sends the 'clock count read' signal, but doesn't actually read the bcm registers until sometime later...)

--- mlu_design/SRC/v1495usr_demo/bcm_integrator.vhd
+++ mlu_design/SRC/v1495usr_demo/bcm_integrator.vhd
@@ -29,6 +29,8 @@ end entity bcm_integrator;
 architecture rtl of bcm_integrator is
 	signal r_sum		: unsigned(63 downto 0)	:= to_unsigned(0, 64);	--Contains running sum
 	signal r_current	: unsigned(31 downto 0)	:= to_unsigned(0, 32);	--Contains latest current
+	signal r_sum_out	: std_logic_vector(63 downto 0);	--Contains last triggered running sum
+	signal r_current_out	: std_logic_vector(31 downto 0);	--Contains last triggered current
 	signal r_old_DV		: std_logic;
 	signal r_old_read	: std_logic;
 
@@ -37,6 +39,8 @@ begin
 	p_edges	: process(i_Clk) is
 	begin
 		if rising_edge(i_Clk) then
+			o_sum <= r_sum_out;
+			o_current <= r_current_out;
 			r_old_DV <= i_DV;
 			r_old_read <= i_read;
 		end if;
@@ -57,11 +61,12 @@ begin
 	p_Read	: process(i_Clk, i_reset) is
 	begin
 		if i_reset = '1' then
-                        o_sum <= std_logic_vector(to_unsigned(0, 64));
+                        r_sum_out <= std_logic_vector(to_unsigned(0, 64));
+                        r_current_out <= std_logic_vector(to_unsigned(0, 32));
 		elsif rising_edge(i_Clk) then
 			if (r_old_read = '0' and i_read = '1') then
-				o_sum <= std_logic_vector(r_sum);
-				o_current <= std_logic_vector(r_current);
+				r_sum_out <= std_logic_vector(r_sum);
+				r_current_out <= std_logic_vector(r_current);
 			end if;
 		end if;
 	end process p_Read;
